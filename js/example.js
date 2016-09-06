2e4b29500d0eb0e2980f05939b4a970838319854
Jacob Thesbjerg <jacobthesbjerg@gmail.com>
2016-09-06 22:35:04 +0200

    Added autofocus on input Added cancel button to go back from adding new item Added conditional logic to check for empty input Small CSS changes for layout purposes

--- js/example.js
+++ js/example.js
@@ -1,17 +1,20 @@
 $(function() {
 
   // SETUP
-  var $list, $newItemForm, $newItemButton;
+  var $list, $newItemForm, $newItemButton, $textInput, $noDescription, $cancelNewItem;
   var item = '';                                 // item is an empty string
   $list = $('ul');                               // Cache the unordered list
   $newItemForm = $('#newItemForm');              // Cache form to add new items
   $newItemButton = $('#newItemButton');          // Cache button to show form
+  $textInput = $('#itemDescription');                   // Cache form input to add new items
+  $noDescription = $('#noDescription');             // Cache alert if no description found
+  $cancelNewItem = $('#cancelNewItem');
 
   $('li').hide().each(function(index) {          // Hide list items
     $(this).fadeIn(1600);     // Then fade them in
   });
 
-  
+
 
   // ITEM COUNTER
   function updateCount() {                       // Create function to update counter
@@ -23,18 +26,35 @@ $(function() {
   // SETUP FORM FOR NEW ITEMS
   $newItemButton.show();                         // Show the button
   $newItemForm.hide();                           // Hide the form
+  $noDescription.hide();
+  $cancelNewItem.hide();                       // Hide the alert
   $('#showForm').on('click', function() {        // When click on add item button
     $newItemButton.hide();                       // Hide the button
-    $newItemForm.show();                         // Show the form
+    $newItemForm.show();                        // Show the form
+    $textInput.focus();                                 // Autofocus on input field
+    $cancelNewItem.show();
   });
 
   // ADDING A NEW LIST ITEM
   $newItemForm.on('submit', function(e) {       // When a new item is submitted
     e.preventDefault();                         // Prevent form being submitted
     var text = $('input:text').val();           // Get value of text input
+    if (text) {                                              // If the text input has a value
     $list.append('<li>' + text + '</li>');      // Add item to end of the list
+    $noDescription.hide();                        // Hide the alert or keep it hidden
     $('input:text').val('');                    // Empty the text input
     updateCount();                              // Update the count
+  } else {
+    $noDescription.show();                      // Show alert if text input does not have a value
+  }
+    $textInput.focus();                     // Focus back to input field
+  });
+
+  $cancelNewItem.on('click', function() {
+    $newItemForm.hide();
+    $noDescription.hide();
+    $cancelNewItem.hide();
+    $newItemButton.show();
   });
 
   // CLICK HANDLING - USES DELEGATION ON <ul> ELEMENT
